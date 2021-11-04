117c1b44553023da5b9f53924f7931c976f4b3ce
Shaunak Warty <shaunak.warty@gmail.com>
2021-11-04 15:48:30 -0700

    Styling changes and add github link

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,15 +1,14 @@
-import { Button, Flex, Heading, Spacer } from '@chakra-ui/react';
+import { Flex, Heading, IconButton, Spacer } from '@chakra-ui/react';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
 
 type NavbarProps = {
   title: string,
-  showAddButton?: boolean,
 };
 
-const oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=897166166269767731&permissions=8&scope=bot%20applications.commands';
-
-export default function AppNavbar({ title, showAddButton }: NavbarProps): JSX.Element {
+export default function AppNavbar({ title }: NavbarProps): JSX.Element {
   const logoSize = { width: 40, height: 40 };
 
   return (
@@ -21,9 +20,15 @@ export default function AppNavbar({ title, showAddButton }: NavbarProps): JSX.El
       </Link>
       <Heading ms={4} size="md">{title}</Heading>
       <Spacer />
-      {showAddButton && (
-        <Button as="a" me={2} href={oauthUrl} target="_blank" bgColor="discord">Add to Discord</Button>
-      )}
+      <IconButton
+        as="a"
+        href="https://github.com/shaunakw/archiver"
+        target="_blank"
+        aria-label="View Source Code"
+        icon={<FontAwesomeIcon icon={faGithub} />}
+        size="xs"
+        variant="link"
+      />
     </Flex>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,17 @@
-import { Code, Flex, Heading, Text } from '@chakra-ui/react';
+import { Button, Code, Flex, Heading, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Navbar from '../components/Navbar';
 
+const oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=897166166269767731&permissions=8&scope=bot%20applications.commands';
+
 const Home: NextPage = () => {
   return <>
-    <Navbar title="" showAddButton />
-    <Flex mt={28} mx={4} direction="column" align="center" textAlign="center">
+    <Navbar title="" />
+    <Flex mt={24} mx="auto" p={4} maxWidth={800} direction="column" align="center" textAlign="center">
       <Heading size="2xl" fontWeight="semibold">Archiver</Heading>
 
       <Text mt={8} fontSize="xl">Securely archive deleted messages in Discord.</Text>
+      <Button as="a" mt={12} href={oauthUrl} target="_blank" bgColor="discord">Add to Discord</Button>
 
       <Heading mt={20} fontWeight="semibold">Commands</Heading>
 
@@ -16,7 +19,7 @@ const Home: NextPage = () => {
       <Text mt={4}>Get a link to the server archive.</Text>
 
       <Code mt={8} fontSize="xl" fontWeight="semibold">/password</Code>
-      <Text mt={4} textStyle="detail" color="gray.400">Requires the Manage Messages permission</Text>
+      <Text mt={4} textStyle="detail" color="gray.400">Required permissions: Manage Messages</Text>
       <Text mt={2}>Get a link to the password generator (will require a 2-factor authentication app like Google Authenticator).</Text>
     </Flex>
   </>;
