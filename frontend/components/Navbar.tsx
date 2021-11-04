5ecbd3cf7566df8ae0e0c75c3db354b52544e41c
Shaunak Warty <shaunak.warty@gmail.com>
2021-11-04 11:41:34 -0700

    Add command description

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,12 +1,15 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, Spacer } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 type NavbarProps = {
   title: string,
+  showAddButton?: boolean,
 };
 
-export default function AppNavbar({ title }: NavbarProps): JSX.Element {
+const oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=897166166269767731&permissions=8&scope=bot%20applications.commands';
+
+export default function AppNavbar({ title, showAddButton }: NavbarProps): JSX.Element {
   const logoSize = { width: 40, height: 40 };
 
   return (
@@ -17,6 +20,10 @@ export default function AppNavbar({ title }: NavbarProps): JSX.Element {
         </a>
       </Link>
       <Heading ms={4} size="md">{title}</Heading>
+      <Spacer />
+      {showAddButton && (
+        <Button as="a" me={2} href={oauthUrl} target="_blank" bgColor="discord">Add to Discord</Button>
+      )}
     </Flex>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,23 @@
-import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+import { Code, Flex, Heading, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
-  const oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=897166166269767731&permissions=8&scope=bot%20applications.commands';
-
   return <>
-    <Navbar title="" />
+    <Navbar title="" showAddButton />
     <Flex mt={28} mx={4} direction="column" align="center" textAlign="center">
       <Heading size="2xl" fontWeight="semibold">Archiver</Heading>
+
       <Text mt={8} fontSize="xl">Securely archive deleted messages in Discord.</Text>
-      <Button as="a" mt={20} href={oauthUrl} target="_blank" bgColor="discord">Add to Discord</Button>
+
+      <Heading mt={20} fontWeight="semibold">Commands</Heading>
+
+      <Code mt={8} fontSize="xl" fontWeight="semibold">/archive</Code>
+      <Text mt={4}>Get a link to the server archive.</Text>
+
+      <Code mt={8} fontSize="xl" fontWeight="semibold">/password</Code>
+      <Text mt={4} textStyle="detail" color="gray.400">Requires the Manage Messages permission</Text>
+      <Text mt={2}>Get a link to the password generator (will require a 2-factor authentication app like Google Authenticator).</Text>
     </Flex>
   </>;
 };
